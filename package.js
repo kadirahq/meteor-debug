3cfe7d40d9a38a9c3a8a2be03b813a6dda832b2d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-06-04 18:43:09 +0530

    Add hot code reload adding

--- package.js
+++ package.js
@@ -24,14 +24,18 @@ function configure(api) {
   api.use('underscore');
   api.use('random');
   api.use('session');
+  api.use('reload');
   api.use('meteorhacks:flow-router@0.0.16', {weak:true});
   api.use('iron:router@1.0.0', {weak:true});
 
   api.addFiles('lib/client/store.js', 'client');
+
   api.addFiles('lib/client/providers/blaze.js', 'client');
   api.addFiles('lib/client/providers/flow_router.js', 'client');
   api.addFiles('lib/client/providers/iron_router.js', 'client');
   api.addFiles('lib/client/providers/ddp.js', 'client');
+  api.addFiles('lib/client/providers/hcr.js', 'client');
+
   api.addFiles('lib/client/connect.js', 'client');
 
   api.addFiles('lib/server/connect.js', 'server');
