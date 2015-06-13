78ba8e4fe194976aff064c5febe6d41b8fb8e534
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-06-13 18:43:18 +0530

    Add blaze tests

--- package.js
+++ package.js
@@ -15,7 +15,10 @@ Package.onTest(function(api) {
   api.use('tinytest');
   api.use('practicalmeteor:sinon@1.14.1_1');
   
+  api.addFiles('tests/utils.js', ['server', 'client']);
+  
   api.addFiles('tests/client/store.js', 'client');
+  api.addFiles('tests/client/providers/blaze.js', 'client');
   api.addFiles('tests/server/integration.js', 'server');
 });
 
@@ -35,6 +38,8 @@ function configure(api) {
   api.use('meteorhacks:flow-router@0.0.16 || 1.0.0', {weak:true});
   api.use('iron:router@1.0.0', {weak:true});
 
+
+  api.addFiles('lib/client/utils.js', 'client');
   api.addFiles('lib/client/store.js', 'client');
 
   api.addFiles('lib/client/providers/blaze.js', 'client');
--- tests/utils.js
+++ tests/utils.js
@@ -1,4 +1,22 @@
-WithNew = function WithNew(original, newMethods, fn) {
+StartStubbing = function(namespace, funcName) {
+  var original = namespace[funcName];
+  var stub = sinon.stub();
+  namespace[funcName] = stub;
+
+  var stopped = false;
+  stub.stop = function() {
+    if(stopped) {
+      return false;
+    }
+
+    namespace[funcName] = original;
+    stopped = true;
+  };
+
+  return stub;
+};
+
+WithNew = function(original, newMethods, fn) {
   var originalMethods = _.clone(original);
   var newKeys = _.difference(_.keys(newMethods), _.keys(original));
   _.extend(original, newMethods);
