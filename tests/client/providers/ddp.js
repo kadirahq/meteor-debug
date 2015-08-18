267b4958f92a8dc3d4c1c7bab074f288ebb91b93
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-08-18 14:51:37 +0530

    Add tests for client side DDP time tracking

--- tests/client/providers/ddp.js
+++ tests/client/providers/ddp.js
@@ -2,12 +2,19 @@ Tinytest.addAsync(
 'Client - DDPProvider - incoming - ready message', 
 function(test, done) {
   var stub = StartStubbing(StoreManager, 'trackEvent');
-  var message = {msg: 'ready', subs: ["1", "2"]};
+  var stub2 = StartStubbing(StoreManager, 'trackTime');
+
+  var message = {msg: 'ready', subs: ['1', '2']};
   var caller = DDPProvider._livedata_data(function(msg) {
     test.equal(msg, message);
     test.equal(stub.args[0], ['ddp-ready', {subs: message.subs}]);
     test.equal(stub.callCount, 1);
     stub.stop();
+    
+    test.equal(stub2.args[0], ['pubsub', '1', 'ready']);
+    test.equal(stub2.args[1], ['pubsub', '2', 'ready']);
+    test.equal(stub2.callCount, 2);
+    stub2.stop();
     done();
   });
 
@@ -161,12 +168,19 @@ Tinytest.addAsync(
 'Client - DDPProvider - incoming - nosub message', 
 function(test, done) {
   var stub = StartStubbing(StoreManager, 'trackEvent');
-  var message = {msg: 'nosub', id: "1", error: "the-error"};
+  var stub2 = StartStubbing(StoreManager, 'trackTime');
+
+  var message = {msg: 'nosub', id: '1', error: 'the-error'};
   var caller = DDPProvider._livedata_nosub(function(msg) {
     test.equal(msg, message);
     test.equal(stub.args[0], ['ddp-nosub', _.omit(message, 'msg')]);
     test.equal(stub.callCount, 1);
     stub.stop();
+
+    test.equal(stub2.args[0], ['pubsub', '1', 'nosub']);
+    test.equal(stub2.callCount, 1);
+    stub2.stop();
+
     done();
   });
 
@@ -177,12 +191,41 @@ Tinytest.addAsync(
 'Client - DDPProvider - incoming - updated message', 
 function(test, done) {
   var stub = StartStubbing(StoreManager, 'trackEvent');
-  var message = {msg: 'updated', methods: ["1", "2"]};
+  var stub2 = StartStubbing(StoreManager, 'trackTime');
+
+  var message = {msg: 'updated', methods: ['1', '2']};
   var caller = DDPProvider._livedata_data(function(msg) {
     test.equal(msg, message);
     test.equal(stub.args[0], ['ddp-updated', {methods: message.methods}]);
     test.equal(stub.callCount, 1);
     stub.stop();
+
+    test.equal(stub2.args[0], ['method', '1', 'updated']);
+    test.equal(stub2.args[1], ['method', '2', 'updated']);
+    test.equal(stub2.callCount, 2);
+    stub2.stop();
+    done();
+  });
+
+  caller(message);
+});
+
+Tinytest.addAsync(
+'Client - DDPProvider - incoming - result message', 
+function(test, done) {
+  var stub = StartStubbing(StoreManager, 'trackEvent');
+  var stub2 = StartStubbing(StoreManager, 'trackTime');
+
+  var message = {msg: 'result', id: '1'};
+  var caller = DDPProvider._livedata_result(function(msg) {
+    test.equal(msg, message);
+    test.equal(stub.callCount, 0);
+    stub.stop();
+
+    test.equal(stub2.args[0], ['method', '1', 'result']);
+    test.equal(stub2.callCount, 1);
+    stub2.stop();
+
     done();
   });
 
@@ -193,14 +236,21 @@ Tinytest.addAsync(
 'Client - DDPProvider - incoming - updated message and ignore some messages', 
 function(test, done) {
   var stub = StartStubbing(StoreManager, 'trackEvent');
-  var message = {msg: 'updated', methods: ["1", "2"]};
-  DDPProvider.ignoringMethodIds = {"2": true};
+  var stub2 = StartStubbing(StoreManager, 'trackTime');
+
+  var message = {msg: 'updated', methods: ['1', '2']};
+  DDPProvider.ignoringMethodIds = {'2': true};
   var caller = DDPProvider._livedata_data(function(msg) {
     test.equal(msg, message);
-    test.equal(stub.args[0], ['ddp-updated', {methods: ["1"]}]);
+    test.equal(stub.args[0], ['ddp-updated', {methods: ['1']}]);
     test.equal(stub.callCount, 1);
-    test.equal(DDPProvider.ignoringMethodIds, {});
     stub.stop();
+
+    test.equal(stub2.args[0], ['method', '1', 'updated']);
+    test.equal(stub2.callCount, 1);
+    stub2.stop();
+
+    test.equal(DDPProvider.ignoringMethodIds, {});
     done();
   });
 
@@ -227,12 +277,18 @@ Tinytest.addAsync(
 'Client - DDPProvider - outgoing - sub message', 
 function(test, done) {
   var stub = StartStubbing(StoreManager, 'trackEvent');
-  var message = {msg: 'sub', id: "1", name: 'the-name'};
+  var stub2 = StartStubbing(StoreManager, 'trackTime');
+
+  var message = {msg: 'sub', id: '1', name: 'the-name'};
   var caller = DDPProvider._send(function(msg) {
     test.equal(msg, message);
     test.equal(stub.args[0], ['ddp-sub', _.omit(message, 'msg')]);
     test.equal(stub.callCount, 1);
     stub.stop();
+
+    test.equal(stub2.args[0], ['pubsub', '1', 'start']);
+    test.equal(stub2.callCount, 1);
+    stub2.stop();
     done();
   });
 
@@ -243,12 +299,18 @@ Tinytest.addAsync(
 'Client - DDPProvider - outgoing - unsub message', 
 function(test, done) {
   var stub = StartStubbing(StoreManager, 'trackEvent');
-  var message = {msg: 'unsub', id: "1"};
+  var stub2 = StartStubbing(StoreManager, 'trackTime');
+
+  var message = {msg: 'unsub', id: '1'};
   var caller = DDPProvider._send(function(msg) {
     test.equal(msg, message);
     test.equal(stub.args[0], ['ddp-unsub', _.omit(message, 'msg')]);
     test.equal(stub.callCount, 1);
     stub.stop();
+
+    test.equal(stub2.args[0], ['pubsub', '1', 'unsub']);
+    test.equal(stub2.callCount, 1);
+    stub2.stop();
     done();
   });
 
@@ -259,13 +321,20 @@ Tinytest.addAsync(
 'Client - DDPProvider - outgoing - method message', 
 function(test, done) {
   var stub = StartStubbing(StoreManager, 'trackEvent');
-  var message = {msg: 'method', id: "1", method: "the-method"};
+  var stub2 = StartStubbing(StoreManager, 'trackTime');
+
+  var message = {msg: 'method', id: '1', method: 'the-method'};
   var caller = DDPProvider._send(function(msg) {
     test.equal(msg, message);
     var info = {name: message.method, id: message.id};
     test.equal(stub.args[0], ['ddp-method', info]);
     test.equal(stub.callCount, 1);
     stub.stop();
+
+    test.equal(stub2.args[0], ['method', '1', 'start']);
+    test.equal(stub2.callCount, 1);
+    stub2.stop();
+
     done();
   });
 
@@ -276,7 +345,7 @@ Tinytest.addAsync(
 'Client - DDPProvider - outgoing - ignoring method message', 
 function(test, done) {
   var stub = StartStubbing(StoreManager, 'trackEvent');
-  var message = {msg: 'method', id: Random.id(), method: "kadira.debug.updateTimeline"};
+  var message = {msg: 'method', id: Random.id(), method: 'kadira.debug.updateTimeline'};
   var caller = DDPProvider._send(function(msg) {
     test.equal(msg, message);
     test.equal(DDPProvider.ignoringMethodIds[message.id], 2);
