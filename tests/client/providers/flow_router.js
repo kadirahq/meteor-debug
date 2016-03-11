1096ee8aa925aeae96cd653803c0f41633a6fbbe
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-03-11 18:53:00 +0530

    Track routes from kadira:flow-router Earlier it was meteorhacks:flow-router

--- tests/client/providers/flow_router.js
+++ tests/client/providers/flow_router.js
@@ -1,27 +1,5 @@
 Tinytest.addAsync(
-'Client - FlowRouterProvider - track with middlewares', 
-function(test, done) {
-  var url = 'the-url';
-  var that = {next: finalize};
-  var stub = StartStubbing(StoreManager, 'trackEvent');
-  var FlowRouter = {
-    middleware: function(cb) {
-      cb(url, finalize);
-    }
-  };
-
-  FlowRouterProvider.track(FlowRouter);
-
-  function finalize() {
-    test.equal(stub.callCount, 1);
-    test.equal(stub.args[0], ['route', {path: url, provider: 'flow-router'}]);
-    stub.stop();
-    done();
-  }
-});
-
-Tinytest.addAsync(
-'Client - FlowRouterProvider - track with triggers', 
+'Client - FlowRouterProvider - track with triggers',
 function(test, done) {
   var url = 'the-url';
   var that = {next: finalize};
